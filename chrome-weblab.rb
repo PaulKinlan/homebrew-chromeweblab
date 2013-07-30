4f3d7f843914cea52a0e83477c69b0cbb009a9a7
Paul Kinlan <paul.kinlan@gmail.com>
2013-07-30 16:56:29 +0100

    Adding support for node

--- chrome-weblab.rb
+++ chrome-weblab.rb
@@ -3,7 +3,7 @@ require 'formula'
 class ChromeWeblab < Formula
    depends_on :python => :recommended
    depends_on :python => ['twisted', 'autobahn', 'txosc', 'PyYAML']
-   depends_on 'node'
+   depends_on :node
    depends_on :cairo
    depends_on :libpng
    depends_on :pkg-config
